4d1e2338c0f4483ecf1f103cf57fc4af5fc03386
Allison Poppe <allisonpoppe89@gmail.com>
2022-12-03 13:23:49 +0100

    Day 03 structure

---
+++ 03/src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+	import Piano from '$lib/piano.svelte';
+</script>
+
+<div class="wrapper">
+	<Piano />
+</div>
+
+<style>
+	.wrapper {
+		align-items: center;
+		display: flex;
+		justify-content: center;
+		height: 100vh;
+		width: 100vw;
+	}
+	:global(svg) {
+		margin: auto;
+	}
+	:global(.white-keys) {
+		fill: white;
+	}
+	:global(.white-keys:hover) {
+		fill: #ffd200;
+	}
+	:global(.black-keys) {
+		fill: black;
+	}
+	:global(.black-keys:hover) {
+		fill: #f40082;
+	}
+	:global(body) {
+		background-color: #37b2c3;
+	}
+</style>
