a459292e47bdae1f060297db5b5019e8033c3ef6
Allison Poppe <allisonpoppe89@gmail.com>
2022-12-04 19:52:35 +0100

    Day 04 structure

---
+++ 04/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import Keyboard from './keyboard.svelte';
+</script>
+
+<div class="keyboard-wrapper">
+  <Keyboard />
+</div>
+
+<style>
+  .keyboard-wrapper {
+    align-items: center;
+    display: flex;
+    justify-content: center;
+  }
+</style>
